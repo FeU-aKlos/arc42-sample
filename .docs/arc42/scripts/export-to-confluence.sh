bc879f51259f217bb57eeb6894e9494846155f44
Andreas Klos <andreas.klos@mmmake.com>
2024-02-28 16:54:10 +0100

    wip

--- .docs/arc42/scripts/export-to-confluence.sh
+++ .docs/arc42/scripts/export-to-confluence.sh
@@ -1,16 +1,10 @@
 ###
 # PREPARE
 ###
-chmod +x ./init.sh
-./init.sh
-
-chmod +x ./scripts/prepare.sh
-./scripts/prepare.sh
-
+bash init.sh
+bash scripts/prepare.sh
 curl -Lo dtcw doctoolchain.github.io/dtcw
-chmod +x ./dtcw
-
-./dtcw install doctoolchain
+bash dtcw install doctoolchain
 
 ###
 # EXPORT
@@ -20,10 +14,9 @@ echo "==========================="
 echo "  start publishToConfluence"
 echo "==========================="
 
-./dtcw publishToConfluence -PbearerToken="$ARC42_EXPORT_CONFLUENCE_USER_BEARER" -PconfluenceUser="$ARC42_EXPORT_CONFLUENCE_USER_EMAIL" -PconfluencePass="$ARC42_EXPORT_CONFLUENCE_USER_PAT" --info
+bash dtcw publishToConfluence -PbearerToken="$ARC42_EXPORT_CONFLUENCE_USER_BEARER" -PconfluenceUser="$ARC42_EXPORT_CONFLUENCE_USER_EMAIL" -PconfluencePass="$ARC42_EXPORT_CONFLUENCE_USER_PAT" --info
 
 ###
 # CLEAN
 ###
-chmod +x  ./scripts/cleanup.sh
-./scripts/cleanup.sh
+bash scripts/cleanup.sh
